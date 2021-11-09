f3e0789c3c69e3d17e1824c2ea5ac124fe7776de
Yuzunoha <studysteadystandby@gmail.com>
2021-11-10 01:31:15 +0900

    morganが効いた

--- express-app/app.js
+++ express-app/app.js
@@ -1,12 +1,14 @@
 var http = require('http');
+var morgan = require('morgan');
 var express = require('express');
 
 var app = express();
 
+app.use(morgan('combined'));
+
 app.get('/', (req, res) => {
-  return res.send('こんにちは');
+  return res.send('こんにちは!');
 });
 
 var server = http.createServer(app);
-
 server.listen(80);
