8ae1762a5c104edf4c401ebd508481f8c224df8b
Yuzunoha <studysteadystandby@gmail.com>
2021-11-10 15:44:36 +0900

    ejsもカンタンやんけ。pugハンズオンしておいてよかった

--- express-app/app.js
+++ express-app/app.js
@@ -6,12 +6,12 @@ var path = require('path');
 var app = express();
 
 app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  return res.render('index', { title: 'こんにちは世界!pug' });
+  return res.render('index', { title: 'こんにちは世界!ejs' });
 });
 
 var server = http.createServer(app);
