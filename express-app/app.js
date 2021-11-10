1fc7ec1112eb2cae5f3d502e580e7d9965b38827
Yuzunoha <studysteadystandby@gmail.com>
2021-11-10 15:41:09 +0900

    pug表示できた

--- express-app/app.js
+++ express-app/app.js
@@ -1,13 +1,17 @@
 var http = require('http');
 var morgan = require('morgan');
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
 
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  return res.send('こんにちは!');
+  return res.render('index', { title: 'こんにちは世界!pug' });
 });
 
 var server = http.createServer(app);
